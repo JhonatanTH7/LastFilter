287978b6665e68925b16bb1ba2e146980075affc
LuisGuilermoF <Guillermoluis414@gmail.com>
2024-07-29 12:42:27 -0500

    update description dtos

--- src/main/java/com/Riwi/LastFilter/api/dto/request/CouponRequest.java
+++ src/main/java/com/Riwi/LastFilter/api/dto/request/CouponRequest.java
@@ -8,6 +8,7 @@ import jakarta.persistence.Column;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -18,16 +19,16 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Builder
 public class CouponRequest {
-    @Column(length = 150)
+    @Size(max = 150)
     @NotNull(message = "The couponcode cannot be empty")
     private String couponCode;
-    @Column(length = 150)
+    @Size(max = 150)
     @NotNull(message = "The description cannot be empty")
     private String description;
     @Min(value = 0)
     @Max(value = 100)
     private Double discount;
-    @Column(length = 150)
+    @Size(max = 150)
     @NotNull(message = "The expiresdate cannot be empty")
     private LocalDateTime expiresDate;
     private Boolean isActive;
--- src/main/java/com/Riwi/LastFilter/api/dto/request/UserRequest.java
+++ src/main/java/com/Riwi/LastFilter/api/dto/request/UserRequest.java
@@ -2,6 +2,7 @@ package com.Riwi.LastFilter.api.dto.request;
 
 import jakarta.persistence.Column;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,13 +13,13 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Builder
 public class UserRequest {
-    @Column(length = 150)
+    @Size(max = 150)
     @NotNull(message = "The name cannot be empty")
     private String name;
-    @Column(length = 150)
+    @Size(max = 150)
     @NotNull(message = "The email cannot be empty")
     private String email;
-    @Column(length = 150)
+    @Size(max = 150)
     @NotNull(message = "The password cannot be empty")
     private String password;
     private Boolean isActive;
