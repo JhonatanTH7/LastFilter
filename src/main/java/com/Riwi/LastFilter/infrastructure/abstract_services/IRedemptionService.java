33944e2f1ad7cfb3625014b05d4e369f7dc924e4
KeiberLLB <leandro1991.9@gmail.com>
2024-07-29 13:33:24 -0500

    save

--- src/main/java/com/Riwi/LastFilter/infrastructure/abstract_services/IRedemptionService.java
+++ src/main/java/com/Riwi/LastFilter/infrastructure/abstract_services/IRedemptionService.java
@@ -1,7 +1,5 @@
 package com.Riwi.LastFilter.infrastructure.abstract_services;
 
-import com.Riwi.LastFilter.infrastructure.abstract_services.HTTPGenerics.Create;
-
 public interface IRedemptionService  {
   
 }
